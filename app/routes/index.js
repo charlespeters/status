ac0db4d537b27393d3354a2d2646df6de15941d5
charles peters <charlespeters42@gmail.com>
2016-07-28 09:29:13 -0700

    user block

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    const u = Ember.$.getJSON('https://api.github.com/users/charlespeters');
+    console.log(u);
+
+    this.set('user', u);
+
+    console.log(this.get('user'));
+
+    return this.get('user');
+  },
   model() {
-    const r = Ember.$.getJSON('https://api.github.com/users/charlespeters/repos');
+    const r = Ember.$.getJSON('https://api.github.com/users/charlespeters/repos?page=1&per_page=999&type=owner&sort=updated');
     return r;
-  }
+  },
 });
