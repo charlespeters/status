ea4e1415aa23779cb78dd93f6e32db7688776c42
charles peters <charlespeters42@gmail.com>
2016-11-02 19:58:32 -0700

    small design updates

--- app/components/card-ui.js
+++ app/components/card-ui.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Component.extend({
-  classNames: ['u-br1', 'u-sh--1']
-});
+  classNames: ['c-Card', 'u-br1', 'u-sh--1', 'u-ov--hid']
+})
